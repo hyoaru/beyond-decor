738ff90c186444c8a0c0bd7b87a7278d18877a62
hyoaru <jjcabreraaaa@gmail.com>
2023-10-02 14:36:19 +0800

    [add] context providers

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import { Lora } from 'next/font/google'
 
 // App imports
+import { SupabaseContextProvider, UserStateContextProvider } from "./context";
 import Footer from './components/Footer'
 import Header from './components/Header'
 import './globals.css'
@@ -19,11 +20,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={typeface.className} suppressHydrationWarning>
-        <Header />
-        <main className="my-3">
-          {children}
-        </main>
-        <Footer />
+        <SupabaseContextProvider>
+          <UserStateContextProvider>
+            <Header />
+            <main className="my-3">
+              {children}
+            </main>
+            <Footer />
+          </UserStateContextProvider>
+        </SupabaseContextProvider>
       </body>
     </html>
   )
