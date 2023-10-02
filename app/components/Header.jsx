66a95732f09cc73bb39319f5ba253163355de1a0
hyoaru <jjcabreraaaa@gmail.com>
2023-10-02 14:38:00 +0800

    [add] user authenticated icon

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,31 @@
-import Link from "next/link"
+"use client"
 
+import Link from "next/link"
+import { useEffect, useRef, useState } from "react"
+import { useSupabaseContext, useUserStateContext } from "../context"
+import { faUser } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useRouter } from "next/navigation"
+
+// App import
 export default function Header() {
+  const supabase = useSupabaseContext()
+  const user = useUserStateContext()
+
+  function handleSignOut(event) {
+    async function signOut(){
+      try {
+        const { error } = await supabase.auth.signOut()
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    event.preventDefault()
+    signOut()
+    window.location.href="/"
+  }
+
   return (
     <>
       <div className="navbar bg-base-100 border-b pb-0">
@@ -35,14 +60,22 @@ export default function Header() {
         </div>
         <div className="navbar-end">
           <button className="btn btn-ghost btn-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-          </button>
-          <button className="btn btn-ghost btn-circle">
             <div className="indicator">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
               <span className="badge badge-xs badge-primary indicator-item"></span>
             </div>
           </button>
+          {user && <>
+            <div className="dropdown dropdown-end">
+              <label tabIndex="0" className="btn btn-ghost btn-circle">
+                <FontAwesomeIcon icon={faUser} size="lg" />
+              </label>
+              <ul tabIndex="0" className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                <li><Link href={"/admin"}>Admin</Link></li>
+                <li><input type="submit" onClick={handleSignOut} value={"Sign out"} /></li>
+              </ul>
+            </div>
+          </>}
         </div>
       </div>
     </>
