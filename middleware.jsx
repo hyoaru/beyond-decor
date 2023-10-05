715da2cce2af3aae5b9c83fe653fc8bbaa5fa003
hyoaru <jjcabreraaaa@gmail.com>
2023-10-05 19:43:26 +0800

    [add] redirect to admin if pathname /admin/login

--- middleware.jsx
+++ middleware.jsx
@@ -8,11 +8,15 @@ export async function middleware(req) {
   const {data: { user }} = await supabase.auth.getUser()
 
   if (user) {
+    if (req.nextUrl.pathname === '/admin/login') {
+      return NextResponse.redirect(new URL('/admin', req.url))
+    }
     return NextResponse.next()
   }
+
   return NextResponse.redirect(new URL('/admin/login', req.url))
 }
 
 export const config = {
-  matcher: ['/admin']
+  matcher: ['/admin/login', '/admin']
 }
