2edeee907c3eda2930641f8986d0222d0932fa58
hyoaru <jjcabreraaaa@gmail.com>
2023-10-08 10:29:02 +0800

    [add] supabase remote pattern

--- next.config.js
+++ next.config.js
@@ -8,6 +8,13 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+
+      {
+        protocol: 'https', 
+        hostname: 'ralndezcjewfbfobbtjy.supabase.co',
+        port: '',
+        pathname: '/**'
+      }
     ],
   },
 }
