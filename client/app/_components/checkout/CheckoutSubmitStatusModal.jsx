28cddd5cab21caf60d54346e7476b820b1f36842
hyoaru <jjcabreraaaa@gmail.com>
2023-11-01 17:01:29 +0800

    [modify] modal looks

--- client/app/_components/checkout/CheckoutSubmitStatusModal.jsx
+++ client/app/_components/checkout/CheckoutSubmitStatusModal.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function CheckoutSubmitStatusModal(props) {
@@ -5,11 +6,15 @@ export default function CheckoutSubmitStatusModal(props) {
   return (
     <>
       <dialog id="CheckoutSubmitStatusModal" className="modal">
-        <div className="modal-box">
-          <h3 className={`font-bold text-lg ${error ? "text-error" : "text-primary"}`}>Checkout submit status</h3>
-          {error 
+        <div className="modal-box text-center py-10">
+          <h1 className={`font-bold text-2xl ${error ? "text-error" : "text-primary"}`}>Checkout submit status</h1>
+          {error
             ? <p className="py-4 text-error">{error.message}</p>
-            : <p className="py-4">"Inquiry has been submitted successfully!</p>
+            : <>
+              <p className="pt-4 font-bold">Inquiry has been submitted successfully!</p>
+              <p>We'll reach out to you <span className='text-primary'>within 1-2 business days</span>. In the mean time, how about you give our works a view?</p>
+              <Link href={"/works"} className='btn btn-primary btn-sm btn-outline mt-5'>View our portfolio</Link>
+            </>
           }
         </div>
         <form method="dialog" className="modal-backdrop">
