1cc7d46c17ab83a167862d24faed426493332a1e
hyoaru <jjcabreraaaa@gmail.com>
2023-11-01 20:05:21 +0800

    [add] config to include pockethost in remote patterns

--- client/next.config.js
+++ client/next.config.js
@@ -21,6 +21,13 @@ const nextConfig = {
         hostname: '127.0.0.1',
         port: '8090',
         pathname: '/**'
+      },
+
+      {
+        protocol: 'https',
+        hostname: 'beyond-decor.pockethost.io',
+        port: '',
+        pathname: '/**'
       }
     ],
   },
