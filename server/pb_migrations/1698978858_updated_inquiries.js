e81e5f138eb1f08f34d5ae4a163bae11f849b31e
hyoaru <jjcabreraaaa@gmail.com>
2023-11-03 10:42:46 +0800

    [modify] api rules inquiries to permit public on create operation

---
+++ server/pb_migrations/1698978858_updated_inquiries.js
@@ -0,0 +1,16 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("bsjz1ge1mr0cuha")
+
+  collection.createRule = ""
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("bsjz1ge1mr0cuha")
+
+  collection.createRule = null
+
+  return dao.saveCollection(collection)
+})
