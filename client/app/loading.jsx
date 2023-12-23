6b90bacc3799ced1a19359e966f7afaf89e92cf8
hyoaru <jjcabreraaaa@gmail.com>
2023-12-23 20:02:50 +0800

    [modify] loading height to occupy 80vh

--- client/app/loading.jsx
+++ client/app/loading.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 export default function Loading() {
   return (
     <>
-      <div className="flex py-52">
-        <span className="mx-auto loading loading-ring loading-lg scale-[1.5]"></span>
+      <div className="h-[80svh] w-full">
+        <div className="flex h-full w-full">
+          <span className="mx-auto my-auto loading loading-ring loading-lg scale-[1.5]"></span>
+        </div>
       </div>
     </>
   )
