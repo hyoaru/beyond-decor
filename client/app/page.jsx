2283158e32663347801365f1f179cafb3b815e0a
hyoaru <jjcabreraaaa@gmail.com>
2023-12-23 19:49:39 +0800

    [fix] redirect link from works to checkout

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -105,7 +105,7 @@ export default async function Home() {
         <AnimationFadeOnShow initialOpacity={0.3}>
           <div className='prose max-w-none px-10 md:px-20'>
             <h1 className='text-center mb-2 mx-6 '>{"Let's start crafting your next priceless memory"}</h1>
-            <Link href={"/works"} className='no-underline'>
+            <Link href={"/checkout"} className='no-underline'>
               <button className="btn btn-primary flex mx-auto mt-6 ">Get a business quote</button>
             </Link>
           </div>
